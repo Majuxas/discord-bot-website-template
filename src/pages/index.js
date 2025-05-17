d1641bffa8aa65b385d32e072f5aaff1f8497f6a
Majuxas <frostezas@gmail.com>
2025-05-17 16:52:12 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <title>Gleemo</title>
         <meta
           name="description"
-          content="Minimal and awesome discord bot website created with ❤️ using NextJS"
+          content="test1"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
